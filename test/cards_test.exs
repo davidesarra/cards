40fe73d0fc4877a453400270c94f35ca414c2ae9
Davide Sarra <davide.sarra@gmail.com>
2020-12-16 13:48:53 +0000

    Test Cards.shuffle

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -6,4 +6,10 @@ defmodule CardsTest do
     deck = Cards.create_deck
     assert length(deck) == 52
   end
+
+  test "shuffle randomizes the order of the cards" do
+    deck = Cards.create_deck
+    shuffled_deck = Cards.shuffle(deck)
+    refute deck == shuffled_deck
+  end
 end
